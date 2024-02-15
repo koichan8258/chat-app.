a8a7e30672a5209b3c20bd0fc065c76201a2284f
koichan8258 <iohikarinohito2@gmail.com>
2024-02-15 14:35:19 +0900

    Active Storageの導入

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,7 @@
 class Message < ApplicationRecord
-<<<<<<< Updated upstream
   belongs_to :room
   belongs_to :user
+  has_one_attached :image
+
   validates :content, presence: true
-=======
->>>>>>> Stashed changes
 end
